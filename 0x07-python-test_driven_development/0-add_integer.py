a80072ce8f4ef56237640fe38ed9304fb0310034
kamalnorman334 <kamalnorman334@gmail.com>
2024-02-02 17:57:18 -0800

    adding integer

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,44 +1,18 @@
 #!/usr/bin/python3
-def add_integer(a, b=98):
-    """
-    Adds two integers.
+"""
+This is the "0-add_integer" module.
+The 0-add_integer module supplies one function, add_integer(a, b).
+"""
 
-    Args:
-        a (int or float): The first number.
-        b (int or float): The second number. Default is 98.
 
-    Returns:
-        int: The addition of a and b.
-
-    Raises:
-        TypeError: If a or b is not an integer or float.
-    """
-    # Check if a is an integer or float
-    if not isinstance(a, (int, float)):
-        raise TypeError("a must be an integer or b must be an integer")
-    
-    # Check if b is an integer or float
-    if not isinstance(b, (int, float)):
-        raise TypeError("a must be an integer or b must be an integer")
-
-    # Return the sum of a and b as an integer
-    return int(a) + int(b)
-
-if __name__ == "__main__":
-    # Test cases
-    print(add_integer(1, 2))
-    print(add_integer(100, -2))
-    print(add_integer(2))
-    print(add_integer(100.3, -2))
-    
-    # Exception handling for invalid input types
-    try:
-        print(add_integer(4, "School"))
-    except Exception as e:
-        print(e)
-    
-    try:
-        print(add_integer(None))
-    except Exception as e:
-        print(e)
-
+def add_integer(a, b):
+    """Return the addition of two numbers."""
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+    return a + b
