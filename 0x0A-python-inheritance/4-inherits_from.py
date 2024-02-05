50fecac1c4ee2c3118c6dc7405ae84a69edb988f
kamalnorman334 <kamalnorman334@gmail.com>
2024-02-05 12:12:26 -0800

    inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-inherits_from = __import__('4-inherits_from').inherits_from
+"""checks if object is an instance of a class that
+inherited from the specified class or not
+"""
 
-a = True
-if inherits_from(a, int):
-    print("{} inherited from class {}".format(a, int.__name__))
-if inherits_from(a, bool):
-    print("{} inherited from class {}".format(a, bool.__name__))
-if inherits_from(a, object):
-    print("{} inherited from class {}".format(a, object.__name__))
+
+def inherits_from(obj, a_class):
+    """Returns true if object is an instance of a class that inherited
+    (directly or indirectly) from the specified class; otherwise False
+    """
+    return (issubclass(type(obj), a_class) and type(obj) != a_class)
--- 0x0A-python-inheritance/4-main.py
+++ 0x0A-python-inheritance/4-main.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""checks if object is an instance of a class that
-inherited from the specified class or not
-"""
+inherits_from = __import__('4-inherits_from').inherits_from
 
-
-def inherits_from(obj, a_class):
-    """Returns true if object is an instance of a class that inherited
-    (directly or indirectly) from the specified class; otherwise False
-    """
-    return (issubclass(type(obj), a_class) and type(obj) != a_class)
+a = True
+if inherits_from(a, int):
+    print("{} inherited from class {}".format(a, int.__name__))
+if inherits_from(a, bool):
+    print("{} inherited from class {}".format(a, bool.__name__))
+if inherits_from(a, object):
+    print("{} inherited from class {}".format(a, object.__name__))
