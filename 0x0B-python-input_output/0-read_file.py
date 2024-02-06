b9ef9910500d9fc3453f2da35ae9361ea93d9953
kamalnorman334 <kamalnorman334@gmail.com>
2024-02-06 11:12:27 -0800

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
+"""
+Contains the read_file function
+"""
+
 
 def read_file(filename=""):
-    """
-    Reads a text file and prints its contents to stdout.
-    :param filename: The name of the file to read. Default is an empty string.
-    """
-    with open(filename, encoding='utf-8') as f:
-        for line in f:
-            print(line, end='')
+    """""reads a text file(UTF8) and prints it to stdout"""
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
